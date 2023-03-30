ce29e8e51149d4dfd4e7fc0b1f031f21133cdab5
Salomon Solomon <ssalomon783@gmail.com>
2023-03-30 11:02:23 +0300

    js file update

--- app.js
+++ app.js
@@ -1,69 +1,21 @@
-let randomNumber1 = Math.floor(Math.random()* 6) + 1;
-let randomNumber2 = Math.floor(Math.random()* 6) + 1;
+      // generate random number for each player
+let randomNumber1 = Math.floor(Math.random()*6)+1;
+let randomNumber2 = Math.floor(Math.random()*6)+1;
+ 
+      // holds images src location as a string 
+let changeImage1 = "images/dice"+ randomNumber1 + ".png";
+let changeImage2 = "images/dice"+ randomNumber2 + ".png";
 
-let randomImage1 = "images/dice" + randomNumber1 + ".png";
-let randomImage2 = "images/dice" + randomNumber2 + ".png";
-// let result = document.querySelector("h1");
+      //changes the player images using the string above
+let player1 = document.querySelectorAll("img")[0].setAttribute("src", changeImage1);
+let player2 = document.querySelectorAll("img")[1].setAttribute("src", changeImage2);
 
-let player1 = document.querySelectorAll("img")[0];
-let player2 = document.querySelectorAll("img")[1];
-
-player1.setAttribute("src", randomImage1);
-player2.setAttribute("src", randomImage2);
-
-    if(randomImage1 > randomImage2 ){
-          document.querySelector("h1").innerHTML = "Player 1  wins!! ⭐";
-    }else if(randomImage2 > randomImage1) {
-          document.querySelector("h1").innerHTML = "Player 2 won!!⭐"
-    }else {
-          document.querySelector("h1").innerHTML = "Draw!!😁"
-    }
-    
-
-
-
-
-// document.querySelector("img .img1").setAttribute("src", randomImage);
-// let player2 = document.querySelector("img .img2");;
-// let result = document.querySelector("h1");
-
-
-
-//     if(player1 > player2){
-//        return result.innerHTML = "player 1 won!!"; 
-//     }else if(player2 > player1){
-//        return result.innerHTML = "player 2 won!!";
-//     }else {
-//         return result.innerHTML = "Draw!!";
-//     }
-
-
-
-
-
-// let diceNumbers = [
-//     "images/dice1.png"
-// ,
-
-//      "images/dice2.png"
-// ,
-
-//      "images/dice3.png"
-// ,
-
-//      "images/dice4.png"
-// ,
-
-//     "images/dice5.png"
-// ,
-
-//     "images/dice6.png"
-// ]
-
-
-//  let  player1 = diceNumbers[Math.floor(Math.random(0, diceNumbers.length)*diceNumbers.length)];
-//    player2 = diceNumbers[Math.floor(Math.random(0, diceNumbers.length)*diceNumbers.length)];
-
-// for(let i = 0; i < diceNumbers.length; i++){
-//         return diceNumbers[index]
-// }
+      // game conditions
+if(randomNumber1 > randomNumber2) {
+      document.querySelector("h1").innerHTML = "Player 1 wins👍"
+}else if(randomNumber1 < randomNumber2) {
+      document.querySelector("h1").innerHTML = "Player 2 wins👍"
+      
+}else {
+      document.querySelector("h1").innerHTML = "Draw😁"
+}
